a52220b47bf8a132df3e922ff7f7b119bb7419e2
SulimanGR <124878939+SulimanGR@users.noreply.github.com>
2024-01-12 20:16:29 +0300

    new js changes
    I made a age checker for practice it check your age if you are more than or equal to 18 you well have access else false.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log("hello");
+function checkAge(age){
+
+    if(age >= 18){
+        return true;
+    }
+    else{
+        return alert('Access Denied');
+    }
+
+}
+
+let age = prompt("how old are you? ")
+
+if(checkAge(age)){
+    alert('Access Granted');
+}
